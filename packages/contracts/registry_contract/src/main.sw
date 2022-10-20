7d537fc70eaed3e338d2fb9147f9620703504d9e
Fossil Frank <fossilfrank@proton.me>
2022-10-20 18:11:30 -0500

    Ensure contracts have same code hash

--- packages/contracts/registry_contract/src/main.sw
+++ packages/contracts/registry_contract/src/main.sw
@@ -4,31 +4,55 @@ use std::{
     storage::StorageMap,
     option::Option,
 };
+use core::num::*;
 use exchange_abi::Exchange;
 
 enum Error {
     UnorderedTokens: (),
+    AlreadyInitialized: (),
+    InvalidContractCode: (),
 }
 
 abi PoolRegistry {
+    #[storage(write, read)]fn initialize(template_exchange_id: b256);
     // Add exchange contract to the token
-    #[storage(write)]fn add_exchange_contract(exchange_id: b256);
+    #[storage(write, read)]fn add_exchange_contract(exchange_id: b256);
     // Get exchange contract for desired token
     #[storage(read)]fn get_exchange_contract(token_a: b256, token_b: b256) -> Option<b256>;
 }
 
+fn get_contract_root(addr: b256) -> b256 {
+    let mut result_buffer: b256 = ~b256::min();
+    asm(hash: result_buffer, addr: addr) {
+        croo hash addr;
+        hash: b256 // Return
+    }
+}
+
 storage {
+    expected_contract_root: b256 = ~b256::min(),
     pools: StorageMap<(b256, b256), b256> = StorageMap {},
 }
 
 impl PoolRegistry for Contract {
-    #[storage(write)]fn add_exchange_contract(exchange_id: b256) {
+    #[storage(write, read)]fn initialize(template_exchange_id: b256) {
+        require(storage.expected_contract_root == ~b256::min(), Error::AlreadyInitialized);
+        let root = get_contract_root(template_exchange_id);
+        storage.expected_contract_root = root;
+    }
+
+    #[storage(write, read)]fn add_exchange_contract(exchange_id: b256) {
         let exchange = abi(Exchange, exchange_id);
+
+        let root = get_contract_root(exchange_id);
+        require(root == storage.expected_contract_root, Error::InvalidContractCode);
+
         let (token0, token1) = exchange.get_tokens();
         require(token0 < token1, Error::UnorderedTokens);
 
         storage.pools.insert((token0, token1), exchange_id);
     }
+
     #[storage(read)]fn get_exchange_contract(token_a: b256, token_b: b256) -> Option<b256> {
         let (token0, token1) = if token_a < token_b { (token_a, token_b) } else { (token_b, token_a) };
         let exchange = storage.pools.get((token0, token1));
--- packages/contracts/registry_contract/tests/harness.rs
+++ packages/contracts/registry_contract/tests/harness.rs
@@ -58,6 +58,13 @@ async fn register_exchange() {
     .await
     .unwrap();
 
+    registry_instance
+        .methods()
+        .initialize(Bits256(exchange_contract_id.hash().into()))
+        .call()
+        .await
+        .unwrap();
+
     // Test storage
     registry_instance
         .methods()
@@ -139,11 +146,89 @@ async fn unordered_tokens_should_fail() {
     .await
     .unwrap();
 
+    registry_instance
+        .methods()
+        .initialize(Bits256(exchange_contract_id.hash().into()))
+        .call()
+        .await
+        .unwrap();
+
     // Test storage
     let is_err = registry_instance
         .methods()
         .add_exchange_contract(Bits256(exchange_contract_id.hash().into()))
         .set_contracts(&[exchange_contract_id.clone()])
+        .call()
+        .await
+        .is_err();
+    assert!(is_err);
+}
+
+#[tokio::test]
+async fn test_invalid_contract() {
+    // Provider and Wallet
+    let wallet = launch_provider_and_get_wallet().await;
+
+    // Get the contract ID and a handle to it
+    let registry_contract_id = Contract::deploy(
+        "out/debug/registry_contract.bin",
+        &wallet,
+        TxParameters::default(),
+        StorageConfiguration::new(None, None),
+    )
+    .await
+    .unwrap();
+
+    let registry_instance = TestRegistryBuilder::new(
+        registry_contract_id.to_string(),
+        wallet.clone(),
+    );
+
+    let token0_slot = Bytes32::from_str("0x0000000000000000000000000000000000000000000000000000000000000000").unwrap();
+    let token1_slot = Bytes32::from_str("0x0000000000000000000000000000000000000000000000000000000000000001").unwrap();    
+
+    // Create fake token ids
+    let token_id_1 = Bytes32::from_str("0x000005877b940cc69d7a9a71000a0cfdd79e93f783f198de893165278712a480").unwrap();
+    let token_id_2 = Bytes32::from_str("0x716c345b96f3c17234c73881c40df43d3d492b902a01a062c12e92eeae0284e9").unwrap();
+    let token_id_nonexistent = Bytes32::from_str("0xdf43d3d492b90716c345b96f3c17234c73881e92eeae0284e9c402a01a062c12").unwrap();
+
+    let storage_vec = vec![
+        StorageSlot::new(token0_slot, token_id_1),
+        StorageSlot::new(token1_slot, token_id_2),
+    ];
+
+    let valid_exchange_contract_id = Contract::deploy_with_parameters(
+        "../exchange_contract/out/debug/exchange_contract.bin",
+        &wallet,
+        TxParameters::default(),
+        StorageConfiguration::with_manual_storage(Some(storage_vec.clone())),
+        Salt::from([1u8; 32]),
+    )
+    .await
+    .unwrap();
+
+    let invalid_exchange_contract_id = Contract::deploy_with_parameters(
+        "./tests/modified_exchange_contract.bin",
+        &wallet,
+        TxParameters::default(),
+        StorageConfiguration::with_manual_storage(Some(storage_vec)),
+        Salt::from([1u8; 32]),
+    )
+    .await
+    .unwrap();
+
+    registry_instance
+        .methods()
+        .initialize(Bits256(valid_exchange_contract_id.hash().into()))
+        .call()
+        .await
+        .unwrap();
+
+    // Test storage
+    let is_err = registry_instance
+        .methods()
+        .add_exchange_contract(Bits256(invalid_exchange_contract_id.hash().into()))
+        .set_contracts(&[invalid_exchange_contract_id.clone()])
         .call()
         .await
         .is_err();
