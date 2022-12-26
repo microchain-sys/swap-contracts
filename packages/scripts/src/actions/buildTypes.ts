895b0aaddbdd06e0ee3d45898a982d41a4b905f0
Anderson Arboleya <anderson@arboleya.me>
2022-12-26 20:07:33 -0300

    Updating types’ generation routine

--- packages/scripts/src/actions/buildTypes.ts
+++ packages/scripts/src/actions/buildTypes.ts
@@ -1,29 +1,12 @@
-import { runTypeChain, glob } from 'fuelchain';
+import { runTypegen } from '@fuel-ts/abi-typegen/runTypegen';
 import type { Config } from 'src/types';
-import { promises as fs } from 'fs';
 
-// Generate types using typechain
-// and typechain-target-fuels modules
+// Generate types using fuels/abi-typegen
 export async function buildTypes(config: Config) {
   const cwd = process.cwd();
-  // find all files matching the glob
-  const allFiles = glob(cwd, [config.types.artifacts]);
-
-  // Hack until https://github.com/FuelLabs/fuels-ts/issues/521 is resolved
-  const searchStr = '"name": "get_tokens",\n      "output": {\n        "name": ""';
-  const replaceStr = '"name": "get_tokens",\n      "output": {\n        "name": "tokens"';
-  for (const filePath of allFiles) {
-    const code = await fs.readFile(filePath, { encoding: 'utf8' });
-    if (code.indexOf(searchStr) !== -1) {
-      fs.writeFile(filePath, code.replace(searchStr, replaceStr));
-    }
-  }
-
-  await runTypeChain({
+  await runTypegen({
     cwd,
-    filesToProcess: allFiles,
-    allFiles,
-    outDir: config.types.output,
-    target: 'fuels',
+    input: config.types.artifacts,
+    output: config.types.output,
   });
 }
